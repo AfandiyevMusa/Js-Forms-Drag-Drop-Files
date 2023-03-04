27fa7fdb5774a827686ea8cc6d2ec6ce08909159
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-03-05 02:11:58 +0400

    hw is updated

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -4,21 +4,28 @@ let button = document.querySelector("i")
 let input = document.querySelector("input")
 let dragarea = document.querySelector(".drag-part");
 let tableBody = document.querySelector("table tbody")
+let dragInput = document.querySelector(".file");
 
 button.addEventListener("click", function () {
-    input.click();
+    dragInput.click();
 })
 
 dragarea.addEventListener("click", function () {
-    input.click();
+    dragInput.click();
 })
 
-input.addEventListener("change", function (e) {
+dragInput.addEventListener("change", function (e) {
     for (const file of e.target.files) {
         let reader = new FileReader();
         reader.onloadend = (event) => {
-            document.querySelector("img").setAttribute("class", "my-5");
-            document.querySelector("img").setAttribute("src", event.currentTarget.result);
+            let base64 = event.currentTarget.result;
+            tableBody.innerHTML += `<tr><td><img src="${base64}"></td><td>${file.name}</td><td>${file.size / 1024} KB</td><td><i class="fa-solid fa-trash delete"></i></td></tr>`;
+            let deleteButton = document.querySelectorAll(".delete")
+            for (let i = 0; i < deleteButton.length; i++) {
+                deleteButton[i].addEventListener("click", function () {
+                    deleteButton[i].parentElement.parentElement.style.display = "none";
+                })
+            }
         }
         reader.readAsDataURL(file);
     }
@@ -35,20 +42,20 @@ dragarea.addEventListener("dragleave", function () {
 
 dragarea.addEventListener("drop", function (e) {
     e.preventDefault();
-    input.addEventListener("change", function (e) {
-        for (const file of e.target.files) {
-            let reader = new FileReader();
-            reader.onloadend = (event) => {
-                let base64 = event.currentTarget.result;
-                tableBody.innerHTML += `<tr><td><img src="${base64}"></td><td>${file.name}</td><td>${file.size / 1024} KB</td><td><i class="fa-solid fa-trash delete"></i></td></tr>`;
-                let deleteButton = document.querySelectorAll(".delete")
-                for (let i = 0; i < deleteButton.length; i++) {
-                    deleteButton[i].addEventListener("click", function () {
-                        deleteButton[i].parentElement.parentElement.style.display = "none";
-                    })
-                }
+    dragarea.classList.remove("hover-part");
+
+    for (const file of e.dataTransfer.files) {
+        let reader = new FileReader();
+        reader.onloadend = (event) => {
+            let base64 = event.currentTarget.result;
+            tableBody.innerHTML += `<tr><td><img src="${base64}"></td><td>${file.name}</td><td>${file.size / 1024} KB</td><td><i class="fa-solid fa-trash delete"></i></td></tr>`;
+            let deleteButton = document.querySelectorAll(".delete")
+            for (let i = 0; i < deleteButton.length; i++) {
+                deleteButton[i].addEventListener("click", function () {
+                    deleteButton[i].parentElement.parentElement.style.display = "none";
+                })
             }
-            reader.readAsDataURL(file);
         }
-    })
+        reader.readAsDataURL(file);
+    }
 })
